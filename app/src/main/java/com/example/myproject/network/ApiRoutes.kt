167a2664876744bc038ed4b844770e6bd703a409
Thomas Fouquert <tfouquert@gmail.com>
2023-04-22 13:09:04 +0200

    success on login

--- app/src/main/java/com/example/myproject/network/ApiRoutes.kt
+++ app/src/main/java/com/example/myproject/network/ApiRoutes.kt
@@ -2,7 +2,7 @@ package com.example.myproject.network
 
 object ApiRoutes {
 
-    const val BASE_URL = "http://127.0.0.1:8000"
+    const val BASE_URL = "http://10.0.2.2:8000"
     const val REGISTER = "/api/users"
     const val LOGIN = "/api/login"
     const val CATEGORY = "/api/categories"
