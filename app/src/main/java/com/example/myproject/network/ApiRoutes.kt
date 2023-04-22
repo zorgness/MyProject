156333791d08c35e29a2cb4d46c9330bd8bc9c90
Thomas Fouquert <tfouquert@gmail.com>
2023-04-22 10:34:01 +0200

    api routes

---
+++ app/src/main/java/com/example/myproject/network/ApiRoutes.kt
@@ -0,0 +1,10 @@
+package com.example.myproject.network
+
+object ApiRoutes {
+
+    const val BASE_URL = "http://127.0.0.1:8000/api"
+    const val REGISTER = "/users"
+    const val LOGIN = "/login"
+    const val CATEGORY = "/categories"
+
+}
