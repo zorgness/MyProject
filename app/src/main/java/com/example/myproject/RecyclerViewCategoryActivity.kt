6d8fd592dd84e30ef83a178f0d93b153d5c729cc
Thomas Fouquert <tfouquert@gmail.com>
2023-04-22 15:02:00 +0200

    add logout

--- app/src/main/java/com/example/myproject/RecyclerViewCategoryActivity.kt
+++ app/src/main/java/com/example/myproject/RecyclerViewCategoryActivity.kt
@@ -1,7 +1,14 @@
 package com.example.myproject
 
+import SHAREDPREF_NAME
+import SHAREDPREF_SESSION_TOKEN
+import SHAREDPREF_SESSION_USERNAME
+import SHAREDPREF_SESSION_USER_ID
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,6 +39,16 @@ class RecyclerViewCategoryActivity : AppCompatActivity() {
 
         myViewModel.categoriesLiveData.observe(this) {categories->
             categoryAdapter.setCategories(categories)
+        }
+
+
+        //LOGOUT
+        with(applicationContext.getSharedPreferences(SHAREDPREF_NAME, Context.MODE_PRIVATE)) {
+            findViewById<Button>(R.id.btn_logout).setOnClickListener {
+                edit().remove(SHAREDPREF_SESSION_TOKEN).remove(SHAREDPREF_SESSION_USERNAME).remove(SHAREDPREF_SESSION_USER_ID).apply()
+                startActivity(Intent(this@RecyclerViewCategoryActivity, LoginActivity::class.java))
+                finish()
+            }
         }
 
     }
