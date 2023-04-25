b12eda351d5fc2d99e72f53c5c95f7e42331319a
Thomas Fouquert <tfouquert@gmail.com>
2023-04-25 21:43:04 +0200

    update on login register and splash fragments

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -8,19 +8,39 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.myproject.databinding.ActivityMainBinding
 import com.example.myproject.fragment.CategoryFragment
 import com.example.myproject.fragment.LoginFragment
 import myToast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.loginFragment || destination.id == R.id.registerFragment || destination.id == R.id.splashFragment) {
+
+                binding.bottomNav.visibility = View.GONE
+            } else {
+
+                binding.bottomNav.visibility = View.VISIBLE
+            }
+        }
 
 
         //LOGOUT
@@ -33,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                     setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                     replace(R.id.nav_host, LoginFragment())
                 }*/
+                navController.navigate(R.id.loginFragment)
 
             }
 
