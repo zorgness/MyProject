7b008555bb1ab0ed1c215fbc1aecaa878b594fce
Thomas Fouquert <tfouquert@gmail.com>
2023-04-25 09:08:23 +0200

    activity event data transfert object

---
+++ app/src/main/java/com/example/myproject/dataclass/ActivityEventDto.kt
@@ -0,0 +1,33 @@
+package com.example.myproject.dataclass
+
+
+import com.squareup.moshi.Json
+
+data class ActivityEventDto(
+    @Json(name = "@context")
+    val context: String,
+    @Json(name = "@id")
+    val idHydrap: String,
+    @Json(name = "@type")
+    val type: String,
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "title")
+    val title: String,
+    @Json(name = "description")
+    val description: String,
+    @Json(name = "location")
+    val location: String,
+    @Json(name = "meeting_point")
+    val meetingPoint: String,
+    @Json(name = "max_of_people")
+    val maxOfPeople: Int,
+    @Json(name = "start_at")
+    val startAt: String,
+    @Json(name = "category")
+    val category: String,
+    @Json(name = "creator")
+    val creator: String,
+    @Json(name = "meetingPoint")
+
+)
