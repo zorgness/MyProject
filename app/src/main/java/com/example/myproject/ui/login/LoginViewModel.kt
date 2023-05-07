f64a5f671bdfffa692f314f823b22d4eee615722
Thomas Fouquert <tfouquert@gmail.com>
2023-05-07 21:51:05 +0200

    update on login view model

--- app/src/main/java/com/example/myproject/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/myproject/ui/login/LoginViewModel.kt
@@ -4,6 +4,7 @@ import ERROR_401
 import ERROR_403
 import STATUS_USER_SUCCESS
 import android.content.Context
+import android.system.ErrnoException
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -18,6 +19,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Response
+import java.net.ConnectException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -44,48 +46,60 @@ class LoginViewModel @Inject constructor(
     fun login() {
 
         if (emailLiveData.value?.isNotBlank() == true && passwordLiveData.value?.isNotBlank() == true) {
-            viewModelScope.launch {
 
-                val responseLogin: Response<SessionDto>? = withContext(Dispatchers.IO) {
-                    apiService.login(LoginInfo(emailLiveData.value!!, passwordLiveData.value!!))
-                }
+            try {
+                viewModelScope.launch {
 
-                val body = responseLogin?.body()
-
-                when {
-                    responseLogin == null -> {
-
-                        _messageLiveData.value = context.getString(R.string.server_error)
+                    val responseLogin: Response<SessionDto>? = withContext(Dispatchers.IO) {
+                        apiService.login(LoginInfo(emailLiveData.value!!, passwordLiveData.value!!))
                     }
 
-                    responseLogin.isSuccessful && (body != null) -> {
+                    val body = responseLogin?.body()
 
-                        if (body.status == STATUS_USER_SUCCESS) {
-
-                            sharedPref.saveToken(body.token)
-                            sharedPref.saveUserId(body.id)
-                            sharedPref.saveUsername(body.username)
-
-                            _messageLiveData.value =
-                                "${context.getString(R.string.welcome)} ${body.username} "
-                            _statusLiveData.value = body.status
-
+                    when {
+                        responseLogin == null -> {
+                            _messageLiveData.value = context.getString(R.string.server_error)
                         }
+
+                        responseLogin.isSuccessful && (body != null) -> {
+
+                            if (body.status == STATUS_USER_SUCCESS) {
+
+                                sharedPref.saveToken(body.token)
+                                sharedPref.saveUserId(body.id)
+                                sharedPref.saveUsername(body.username)
+
+                                _messageLiveData.value =
+                                    "${context.getString(R.string.welcome)} ${body.username} "
+                                _statusLiveData.value = body.status
+
+                            }
+                        }
+                        responseLogin.code() == ERROR_401 ->
+                            _messageLiveData.value = context.getString(R.string.wrong_credential)
+
+                        responseLogin.code() == ERROR_403 ->
+                            _messageLiveData.value = context.getString(R.string.unauthorized)
                     }
-                    responseLogin.code() == ERROR_401 ->
-                        _messageLiveData.value = context.getString(R.string.wrong_credential)
-
-                    responseLogin.code() == ERROR_403 ->
-                        _messageLiveData.value = context.getString(R.string.unauthorized)
                 }
-
             }
-
-        } else
+            catch (e: ConnectException) {
+                _messageLiveData.value = context.getString(R.string.no_connection)
+            }
+            catch (erno: ErrnoException) {
+                _messageLiveData.value = context.getString(R.string.no_connection)
+            }
+        }
+        else {
             _messageLiveData.value = context.getString(R.string.empty_fields)
-
+        }
 
     }
 
-
 }
+
+
+
+
+
+
